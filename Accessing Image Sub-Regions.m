b01880e6897f713259e3da6902b9e5e4c9085219
Rakshith N V <41533982+rakshithnv@users.noreply.github.com>
2020-03-28 09:52:23 +0530

    Create Accessing Image Sub-Regions.m

---
+++ Accessing Image Sub-Regions.m
@@ -0,0 +1,8 @@
+img = imread('cameraman.tif');
+subimg1 = img(1:50,1:50);
+subimg2 = img(  end -49 :end,  end -49 :end);
+SSD = sum(sum((double(subimg1) - double(subimg2)).^2));
+disp(SSD)
+
+
+%accessing top left corner and bottom right corner and computing sum of squared differences.
